2e2c167230de268de17bcecc8f94f82e777d0376
xcoolwunmi <wiseone207@gmail.com>
2025-09-11 17:32:24 +0100

    java

---
+++ practice/index.js
@@ -0,0 +1,9 @@
+let person ={
+    name: "solomon",
+    age:35,
+    location: "Ado-Ekiti"
+}
+
+
+
+console.log (`My name is ${person.name}, I am ${person.age} years old, I live in ${person.location}. `)
