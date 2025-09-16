77335a68e754cd4ae41f1e92b1f3daccb63d1e6d
xcoolwunmi <wiseone207@gmail.com>
2025-09-16 17:07:36 +0100

    edited index.js

--- practice/index.js
+++ practice/index.js
@@ -1,9 +1,102 @@
-let person ={
-    name: "solomon",
-    age:35,
-    location: "Ado-Ekiti"
-}
+// let name= "olawande";
+// let age = 35;
+
+
+// console.log(age)
+
+// let number = 25;
+
+// addtion = number ** 5
+
+// console.log(addtion)
+
+// console.log(typeof (person.name))
+
+
+// let online =  true
+// let offline = false
+
+
+// let person ={
+//     name: "sola",
+//     age : 76,
+//     location: "united kingdom"
+// }
+
+// console.log(person.name.length)
+
+
+// let number1 = 7
+// let number2 = 8
+// let number3 = 10
+
+// console.log(number1===number2) //equals to and equal type
+// console.log(number1==number2)// equals to 
+// console.log(number1!=number2) //not equals to 
+// console.log(number1 > number2) //less than and greater than 
+// console.log(number1>=number2) 
+
+// console.log (number1 > number2 && number2 == number3) //and operation
+// console.log (number1 > number2 || number2 < number3) or operation
+// console.log(!(number1<number2)) opposite
 
 
 
-console.log (`My name is ${person.name}, I am ${person.age} years old, I live in ${person.location}. `)
+// // if statement
+// let year = 2000
+
+// if (year>2023) {
+//     console.log("Are you from the future?") 
+// } 
+// else if (year<2023) {
+//     console.log('Are you from the past?')
+// }
+
+// else{
+//     console.log("Yes we are in 2023")
+// }
+
+
+
+
+
+// let text = "JavaScript";
+// let result = text.slice(4);
+// console.log(result); 
+
+
+// ternary operators
+
+// const iAmHungry = true
+
+// const action = iAmHungry? "Go and eat immediatly" : "Go have fun"
+
+// console.log(action);
+
+
+// loops
+
+// for (let i = 0; i <= 20; i++) {
+   
+//     if(i % 2 === 0 && i > 0){
+//         console.log(i)
+//     }
+// }
+
+
+// for (let i = 0; i <= 52; i++) {
+//  if (i === 45){
+//     console.log("I found number 45") 
+//     break
+//  }
+//     console.log('Here is the number ' + i)
+// }
+
+// multiplication table
+
+// let number = 8
+
+// for (let i = 1; i <= 12; i++) {
+//    console.log(`${number} x ${i} = ${number * i}`)
+    
+// 
