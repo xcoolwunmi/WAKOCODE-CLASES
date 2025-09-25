54f4129e6924c27c19d2fa51c4bb2ab60a68172e
xcoolwunmi <wiseone207@gmail.com>
2025-09-25 18:13:17 +0100

    count app

--- counting_app/index.js
+++ counting_app/index.js
@@ -1,3 +1,18 @@
-function increment(){
-    console.log("The button was clicked")
-}
+const countEl = document.querySelector("#count");
+const incrementBtn = document.querySelector("#increment");
+const saveBtn = document.querySelector("#savebtn");
+
+let count = 0
+
+incrementBtn.addEventListener("click", ()=>{
+    count ++;
+    countEl.textContent = count
+
+});
+
+saveBtn.addEventListener("click",()=>{
+    let savedText = " " + count + " -";
+  historyEl.textContent += savedText; 
+  count = 0; 
+  countEl.textContent = count; 
+})
