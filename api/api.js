48226a7d6d1824dc0623c16d6e14f84df1ce102d
xcoolwunmi <wiseone207@gmail.com>
2025-09-30 16:41:57 +0100

    api

--- api/api.js
+++ api/api.js
@@ -1,18 +1,40 @@
-const showsContainer = document.querySelector(".container")
-const bodyEl = document.body
+const showsContainer = document.querySelector(".container");
+const bodyEl = document.body;
+const nextPage = document.querySelector("#nxtpage")
+const previous = document.querySelector("#Previous")
 
-async function basicShowInfo(){
-    const res = await axios.get(`https://www.episodate.com/api/most-popular?page=1`)
-    const shows = res.data.tv_shows
+async function basicShowInfo(num) {
+    showsContainer.innerHTML= ""
+  const res = await axios.get(
+    `https://www.episodate.com/api/most-popular?page=${num}`
+  );
+  const shows = res.data.tv_shows;
 
-    for(let show of shows){
-        const showCard = document.createElement("div")
-        const showImg = document.createElement("img")
-        const h2 = document.createElement("h2")
-        h2.textContent = show.name
-        showCard.append(h2)
-        showsContainer.append (showCard)
-    }
+  for (let show of shows) {
+    const showCard = document.createElement("div");
+    const showImg = document.createElement("img");
+    const h3 = document.createElement("h3");
+    h3.textContent = show.name
+    showImg.src = show.image_thumbnail_path;
+    showCard.append(showImg)
+    showCard.append(h3);
+    showsContainer.append(showCard);
+  }
 }
 
 basicShowInfo();
+
+let numPage = 1;
+
+basicShowInfo (numPage)
+
+nextPage.addEventListener("click", ()=>{
+    numPage += 1;
+    basicShowInfo(numPage)
+})
+
+
+previous.addEventListener("click", ()=>{
+    numPage -= 1;
+    basicShowInfo(numPage)
+})
